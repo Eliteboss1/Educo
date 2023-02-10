2c152ed7380e6cbebcafe8867c4548cc2dbb62cd
Ebiperre Iyoro <iyorop@yahoo.com>
2023-02-10 15:28:01 +0100

    Feat: Updated Nav styling

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -1,44 +1,70 @@
 import React from 'react'
-import {HiOutlineMagnifyingGlass} from "react-icons/hi2";
+import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
 import { prettyGirl } from "../../utils";
 
 const Hero = () => {
-  return (
-    <div className='w-full h-screen bg-white-200 flex flex-col justify-between font-[400] font-[poppins leading-[116%]'>
-       <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
-        <div className='flex flex-col  md:items-start w-full px-2 py-8'>
-            <h2 className='text-5xl max-w-lg font-bold'>
-                Learn From Anywhere in the World
-            </h2>
-             <p className='py-5 leading-7 text-sm max-w-xs'>
-                We provide quality online courses with proper guidance.
-                Our mentors are highly skilled and experienced in their fields
-            </p>
-            <div className="flex items-center">
-            <div className="flex border border-black-200 rounded-xl">
-                <input
-                    type="text"
-                    className="block item-left px-3 w-96 py-2 bg-white border rounded-l-xl border-black"
-                    placeholder="Search for courses"
-                />
-                <button className="px-3 py-3 text-white bg-black border rounded-r-xl ">
-                   <HiOutlineMagnifyingGlass />
-                </button>
+    return (
+        <section>
+            {/* <div className='w-full h-screen bg-white-200 flex flex-col justify-between font-[400] font-[poppins leading-[116%]'>
+                <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
+                    <div className='flex flex-col  md:items-start w-full px-2 py-8'>
+                        <h2 className='text-6xl w-auto font-VisbyBold'>
+                            Learn From Anywhere in the World
+                        </h2>
+                        <p className='py-5 leading-7 text-lg max-w-xs visbyRegular'>
+                            We provide quality online courses with proper guidance.
+                            Our mentors are highly skilled and experienced in their fields
+                        </p>
+                        <div className="flex items-center">
+                            <div className="flex border border-black-200 rounded-xl">
+                                <input
+                                    type="text"
+                                    className="block item-left px-3 w-96 py-2 bg-white border rounded-l-xl border-black"
+                                    placeholder="Search for courses"
+                                />
+                                <button className="px-3 py-3 text-white bg-black border rounded-r-xl ">
+                                    <HiOutlineMagnifyingGlass />
+                                </button>
+                            </div>
+                        </div>
+                        <h3 className="pt-5 pb-3 font-bold">
+                            400,000
+                        </h3>
+                        <p>
+                            Users Trust Us
+                        </p>
+                    </div>
+                    <div>
+                        <img src={prettyGirl} className="max-w-xl" />
+                    </div>
+                </div>
+            </div> */}
+
+            <div>
+                {/* wrapper */}
+                <div>
+                    {/* container */}
+                    <div className="flex px-28 py-16 justify-center">
+                        {/* left side: text and input field */}
+                        <div className="flex flex-col gap-8">
+                            <h1 className="text-6xl font-VisbyBold">Learn From Anywhere in the World</h1>
+                            <p className="font-visbyRegular text-lg">We provide quality online courses with proper<br /> guidance. Our mentors are highly skilled and<br /> experienced in their fields</p>
+                            <div className="flex items-center">
+                                <input type="text" placeholder="Search for courses" className="border-black border-2 w-[576px] h-[80px] rounded-lg rounded-r-none" />
+                                <button className="bg-black text text-white p-3 rounded-lg rounded-l-none"><HiOutlineMagnifyingGlass /></button>
+                            </div>
+                        </div>
+
+
+                        {/* right side: image */}
+                        <div className="">
+                            <img src={prettyGirl} alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <h3 className="pt-5 pb-3 font-bold">
-           400,000
-        </h3>
-        <p>
-           Users Trust Us
-        </p>
-        </div>
-        <div>
-             <img src={prettyGirl} className="max-w-xl"/>
-        </div>
-        </div> 
-    </div>
-  )
+        </section>
+    )
 }
 
 export default Hero
