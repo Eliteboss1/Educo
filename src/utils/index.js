6065342c726eb1075f2e6d47350f458c7b1a6b89
Ebiperre Iyoro <iyorop@yahoo.com>
2023-02-10 23:22:14 +0100

    Feat: Added image to the CTA section
    Reminder: Talk to design team about image placement and size.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,7 @@
 import navLogo from "../assets/images/educo.png";
 import Fotlogo from "../assets/icons/educo.svg";
 import prettyGirl from "../assets/images/prettygirl.png";
+import LadyCta from "../assets/images/ladycta.png"
 
 import Course from "../assets/icons/courses.png"
 import Lifetime from "../assets/icons/lifetime.png"
@@ -8,4 +9,5 @@ import Support from "../assets/icons/support.png"
 import Value from "../assets/icons/value.png"
 
 
-export { navLogo, Fotlogo, prettyGirl, Course, Lifetime, Support, Value };
+
+export { navLogo, Fotlogo, prettyGirl, Course, Lifetime, Support, Value, LadyCta };
