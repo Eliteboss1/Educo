a6e1367ac9e5164f50bd6f61cca71697e4f9d9ee
Ebiperre Iyoro <iyorop@yahoo.com>
2023-02-11 00:04:26 +0100

    Feat: Started Courses section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from "../src/components/Navigation/index";
 import Footer from "../src/components/Footer/index";
 import HeroSection from "../src/components/HeroSection/Hero";
 import CalltoAction from "./components/CTA/CalltoAction";
+import Courses from "./components/Courses";
 export default function App() {
   return (
     <div className="overflow-hidden">
@@ -18,6 +19,11 @@ export default function App() {
       {/* CTA */}
       <div>
         <CalltoAction/>
+      </div>
+
+      {/* Courses */}
+      <div>
+        <Courses/>
       </div>
       {/*Footer*/}
       {/* <div>
