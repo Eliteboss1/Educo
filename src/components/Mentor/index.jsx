2c9dd4b5fd961c4ea3deb9d8a13a78e000f023ad
Ebiperre Iyoro <iyorop@yahoo.com>
2023-02-12 22:10:35 +0100

    Feat: update on mentor

--- src/components/Mentor/index.jsx
+++ src/components/Mentor/index.jsx
@@ -3,21 +3,21 @@ import { learningimg, mentorimg } from "../../utils";
 
 const mentor = () => {
   return (
-    <div className="font-visbyRegular px-24">
+    <div className="font-visbyRegular font-medium px-24">
       <div className="flex">
         <div className="flex-1">
           <img src={mentorimg} />
         </div>
-        <div className="flex-1  ">
+        <div className="flex-1  pl-20">
           <h1 className="text-5xl font-bold leading-[61.34px] ">
             Want to share your Knowledge? Join as a Mentor
           </h1>
-          <p className="pt-8 font-medium    text-[20px]">
+          <p className="pt-8 font-VisbyRegular font-light text-xl">
             We provide quality online courses with proper guidance. Our mentors
             are highly skilled and experienced in their fields
           </p>
           <div className="pt-8">
-            <button className=" bg-black text-white text-2xl font-medium  h-16  w-[237px]   rounded-[10px] border-[1px] border-solid border-black">
+            <button className=" bg-black text-white text-2xl font-light  h-16  w-[237px] rounded-[10px] border-[1px] border-solid border-black">
               Become a Mentor{" "}
             </button>
           </div>
